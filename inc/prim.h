fbb7361d1bc7bb37ea8b7cde9834e049df0f7f33
agorecki6 <agorecki3@gmail.com>
2016-05-01 22:42:32 -0700

    Add license information

--- inc/prim.h
+++ inc/prim.h
@@ -1,3 +1,20 @@
+//==============================================================================
+// This file is part of Gc-Base-Lib.
+// Copyright (C) 2018 Anthony Gorecki <agorecki3@gmail.com>
+//
+// Gc-Base-Lib is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// Gc-Base-Lib is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Gc-Base-Lib. If not, see <http://www.gnu.org/licenses/>.
+//==============================================================================
 #pragma once
 
 #include <macro/all.h>
@@ -9,18 +26,18 @@
 //==============================================================================
 // Enumerations
 //==============================================================================
-enum pt_type {
-	PT_U1 = 0,
-	PT_U2,
-	PT_U4,
-	PT_U8,
-	PT_U16,
-	PT_S1,
-	PT_S2,
-	PT_S4,
-	PT_S8,
-	PT_S16,
-	_PT_COUNT
+enum prim_type {
+	PRIM_U1 = 0,
+	PRIM_U2,
+	PRIM_U4,
+	PRIM_U8,
+	PRIM_U16,
+	PRIM_S1,
+	PRIM_S2,
+	PRIM_S4,
+	PRIM_S8,
+	PRIM_S16,
+	_PRIM_COUNT
 };
 
 
